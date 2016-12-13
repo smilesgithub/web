c7eb85310b06a29a7b1258f3fb25b52fa2820885
smilesgithub <sha_zhang@qq.com>
2016-12-13 22:36:00 +0800

    函数

---
+++ 1213/003.js
@@ -0,0 +1,19 @@
+if (scoops==3) {
+	alert("icecream is running low!");
+} else if (scoops>9) {
+	alert("East faster,the icecream is going to melt");
+} else if (scoops==2) {
+	alert("Going once!");
+} else if (scoops==1) {
+	alert("Going twice!") {
+} else if (scoops==0) {
+	alert ("Gone");
+} else {
+	alert("still lost of icecream left,come and get it");
+}
+
+var scoops=10;
+while (scoops>=0) {
+	scoops=-1;
+}
+alert("life without icecream is not the same");
