409c0e0ecb9ea24dd155b7d1feee9d82f94fbfef
smilesgithub <sha_zhang@qq.com>
2016-12-17 00:12:45 +0800

    this 引用

---
+++ 1216/02.js
@@ -0,0 +1,21 @@
+var o1={
+	name:"1"
+}
+
+var o2={
+	name:"2"
+}
+
+
+
+function robot(name) {
+	this.name=name;
+}
+
+var r1=new robot("1");
+console.log(r1.name);
+
+var r2=new robot("2");
+var r3=new robot("3");
+
+
