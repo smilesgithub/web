260d3eb0ea698a5c7d23b570f74e016919b96095
smilesgithub <sha_zhang@qq.com>
2016-12-18 22:58:37 +0800

    对象函数

---
+++ 1218/001.js
@@ -0,0 +1,17 @@
+// var robot=new object();
+// robot.name="cat";
+// robot.age=1;
+// robot.info={height:120,weight:40}
+// robot.say_hello=fnctiong() {console.log("hello")};
+
+var player = {
+	code:2,
+	name:'Tom',
+	status:'active',
+	score:[
+				7,
+				5,
+				9,
+				5
+			]
+}
---
+++ 1218/002.js
@@ -0,0 +1,14 @@
+// var robot= {
+// 	        x:2,
+// 	say_hello:function() {
+// 		console.log("hello");
+// 	}
+// }
+// robot.say_hello();
+
+var robot=new object();
+robot.say_hello=function() {
+		console.log("hello");
+	}
+}
+robot.say_hello();
