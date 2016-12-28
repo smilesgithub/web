64bb05df059c7d315cc5ea4eb5b9420e22fc0576
smilesgithub <sha_zhang@qq.com>
2016-12-28 22:53:34 +0800

    if结构

---
+++ 1228/001.js
@@ -0,0 +1,14 @@
+var un = undefined;
+var nu = null;
+var num = 123;
+var boolean = false;
+var str = "";
+
+var obj = {name:"sha",age:15,girl:true};
+
+var array = ["China","Japan","America"]
+
+var fun = function(a) {
+	console.log(a);
+}
+fun(obj.name);
