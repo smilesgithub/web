5f5707d1199f98c1c47eb6c8e0d09c4163b38bf4
smilesgithub <sha_zhang@qq.com>
2017-01-03 22:43:41 +0800

    函数声明

---
+++ 0103/001.js
@@ -0,0 +1,14 @@
+var num1 = 3;
+var num2 = 5;
+function compare(a,b) {
+	return a>b ? "bigger":"less";
+}
+result = compare(num1,num2);
+console.log(num1 + "is" + result + "than" + num2) ;
+
+
+function add(a,b) {
+	var result = a+b;
+	return result;
+}
+	var sum = add(1,2);
