e516913662907c7883948fca6611113e7f81a487
smilesgithub <sha_zhang@qq.com>
2017-01-18 23:10:49 +0800

    js获取时间

---
+++ 0118/01.js
@@ -0,0 +1,8 @@
+var myDate = new Date();
+console.log(myDate.getFullYear());
+console.log(myDate.getMonth());
+console.log(myDate.getDate());
+console.log(myDate.getHours());
+console.log(myDate.getMinutes());
+console.log(myDate.getSeconds());
+console.log(myDate.getDay());
